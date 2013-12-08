1ea240ac812f4332628a9b3c4ca6f69c59e11e53
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2013-12-08 14:32:17 +0900

    fix broken spec

--- spec/models/rvm_wrapper_spec.rb
+++ spec/models/rvm_wrapper_spec.rb
@@ -3,8 +3,8 @@ require 'rvm_wrapper'
 
 describe RvmWrapper do
   describe '#initialize' do
-    it { expect(described_class.new({}).impl).to be_nil }
-    it { expect(described_class.new('impl' => '').impl).to be_nil }
+    it { expect(described_class.new({}).impl).to eq('.') }
+    it { expect(described_class.new('impl' => '').impl).to eq('.') }
     it { expect(described_class.new('impl' => '.').impl).to eq('.') }
   end
 end
