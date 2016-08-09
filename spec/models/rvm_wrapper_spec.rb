888eb0bbcd6dd8024e9a37217df623e4c1adbeb1
Eito Katagiri <eitoball@gmail.com>
2016-08-10 08:59:02 +0900

    add spec when job name contains spaces

--- spec/models/rvm_wrapper_spec.rb
+++ spec/models/rvm_wrapper_spec.rb
@@ -19,4 +19,42 @@ describe RvmWrapper do
       expect(described_class).to be_transient('rvm_path')
     end
   end
+
+  describe '#setup' do
+    let(:wrapper) { described_class.new('impl' => '.') }
+    let(:build) { double('build', native: '') }
+    let(:launcher) { double('launcher') }
+    let(:listener) { double('listener', native: '', :<< => '') }
+
+    before do
+      allow(TokenMacro).to receive(:expandAll)
+
+      expect(launcher).to receive(:execute).with('bash', '-c', 'export', {:out => an_instance_of(StringIO)}).once.and_return(0)
+    end
+
+    context 'when rvm is not installed' do
+      before do
+        allow(launcher).to receive(:execute).with('bash', '-c', /\Atest -f/).exactly(4).and_return(-1)
+        allow(launcher).to receive(:execute).with('bash', '-c', /\A source /, an_instance_of(Hash)).once.and_return(0)
+      end
+
+      context 'workspace is in path that contains space' do
+        before do
+          file_path = double('java.hudson.FilePath')
+          allow(file_path).to receive(:+).and_return(file_path)
+          allow(file_path).to receive(:chmod)
+          allow(file_path).to receive(:copyFrom)
+          allow(file_path).to receive(:native).and_return(file_path)
+          allow(file_path).to receive(:read).and_return('')
+          allow(file_path).to receive(:realpath).and_return('a b')
+          allow(build).to receive(:workspace).and_return(file_path)
+        end
+
+        it 'should escape installer real path' do
+          expect(launcher).to receive(:execute).with("a\\ b", {:out => anything})
+          wrapper.setup(build, launcher, listener)
+        end
+      end
+    end
+  end
 end
